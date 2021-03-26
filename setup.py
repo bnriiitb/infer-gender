038cad4913f88359971b2dc24db2f05735dee68f
Naga <nagaraju.iiit@gmail.com>
2021-03-27 01:24:45 +0530

    added predict.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="infer-gender",
-    version="0.1.0.1.2",
+    version="0.1.0",
     description="Infers gender from an Indian first name or full name",
     long_description=README,
     long_description_content_type="text/markdown",
