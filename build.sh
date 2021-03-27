d0eaa97346f8ba63c3d07113240de913ef58ed3b
Naga <nagaraju.iiit@gmail.com>
2021-03-27 15:48:01 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ rm -r infer_gender.egg-info
 rm -r dist
 python setup.py sdist bdist_wheel
 twine check dist/*
-twine upload --repository testpypi dist/*
+#twine upload --repository testpypi dist/*
 twine upload --repository pypi dist/*
